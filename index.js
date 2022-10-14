6a7e4869104de01bfa6b942357925e3aadf77308
Matthew Wingco <mwingco@gmail.com>
2022-10-13 20:14:22 -0700

    working on it

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-//Your Code here
+let firstanswer = window.prompt('Do you head left or right?')
+if(firstanswer === 'left'){
+    let secondanswer = window.prompt(`You come across a stray cat. 
+    It scampers off down a small hole, just large enough for you to crawl through. 
+    Do you follow it, or continue on your path?`)
+} else if(firstanswer === 'right') {
+    let secondanswer = window.prompt(`You come across a snoring dragon. 
+    On the other side of him, you see a shiny chest of treasure. Another path would 
+    lead you away from the dragon altogether. Which path do you take?`)
+}
+if(secondanswer === 'follow'){
+    let thirdanswer = window.prompt(`You follow the cat to a colony of cats, 
+    nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. 
+    They are content with you staying, 
+    but you wonder if you should alert the world to this magical safe haven.`)
+} else if(secondanswer === 'continue'){
+let thirdanswer = window.prompt(`You come across a chamber that reaches upward to a shining light above. 
+There is a long, winding staircase, and a much quicker, 
+but rickety-looking ladder that leads up toward the light. 
+Which do you take?`)
+}
+
