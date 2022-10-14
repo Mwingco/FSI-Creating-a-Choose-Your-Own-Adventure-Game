65f3eb4120300858c7ced39324eb9d4a3192a834
Matthew Wingco <mwingco@gmail.com>
2022-10-13 21:24:14 -0700

    changed a couple things

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-let firstanswer = window.prompt('Do you head left or right?')
-if(firstanswer === 'left'){
-    let secondanswer = window.prompt(`You come across a stray cat. 
+let answer = window.prompt('Do you head left or right?')
+if(answer === 'left'){
+    let answer = window.prompt(`You come across a stray cat. 
     It scampers off down a small hole, just large enough for you to crawl through. 
     Do you follow it, or continue on your path?`)
 } else if(firstanswer === 'right') {
-    let secondanswer = window.prompt(`You come across a snoring dragon. 
+    let answer = window.prompt(`You come across a snoring dragon. 
     On the other side of him, you see a shiny chest of treasure. Another path would 
     lead you away from the dragon altogether. Which path do you take?`)
-}
-if(secondanswer === 'follow'){
-    let thirdanswer = window.prompt(`You follow the cat to a colony of cats, 
+} 
+if(answer === 'follow'){
+    let answer = window.prompt(`You follow the cat to a colony of cats, 
     nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. 
     They are content with you staying, 
     but you wonder if you should alert the world to this magical safe haven.`)
-} else if(secondanswer === 'continue'){
-let thirdanswer = window.prompt(`You come across a chamber that reaches upward to a shining light above. 
+} else if(answer === 'continue'){
+let answer = window.prompt(`You come across a chamber that reaches upward to a shining light above. 
 There is a long, winding staircase, and a much quicker, 
 but rickety-looking ladder that leads up toward the light. 
 Which do you take?`)
 }
-
